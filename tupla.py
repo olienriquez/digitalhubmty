f440b85c0c55bd96476163b536517cafce442c02
olienriquez <olienriquez>
2022-09-07 16:55:29 +0000

    upload "tupla" activity
    upload "tupla" activity

---
+++ tupla.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+tupla = (-4589, "Hola", [1,2,3,4] , 2 + 3j, {"a":1,"b":2})
+tupla
+
+# COMMAND ----------
+
+lista = list(tupla)
+lista
+
+# COMMAND ----------
+
+import numpy
+numpy.arange(1,6)
+
+# COMMAND ----------
+
+diccionario = dict(zip(numpy.arange(1,6),lista))
+diccionario
+
+# COMMAND ----------
+
+
