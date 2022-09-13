2a3ae7d4f4580a666aa247eb0dce167d5aa285af
olienriquez <olienriquez>
2022-09-13 22:03:12 +0000

    Added activity Arreglos Unidimensionales

---
+++ Arreglos Unidimensionales.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 1.- Crea un vector con valores dentro del rango 3 a el número que representa tu edad.
+# MAGIC 
+# MAGIC 2.- Crea un arreglo con los siguientes elementos: [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+# MAGIC 
+# MAGIC 3.- Ordena de forma ascendiente dicho arreglo.
+
+# COMMAND ----------
+
+import numpy as np
+vector = np.arange(3,36)
+vector
+
+# COMMAND ----------
+
+arreglo = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+arreglo
+
+# COMMAND ----------
+
+sorted(arreglo)
+
+# COMMAND ----------
+
+
