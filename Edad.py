f2b5e6b33c8055659ab4668b49fdbafa470afb49
olienriquez <olienriquez>
2022-09-16 23:14:03 +0000

    Added actividad "Edad"

---
+++ Edad.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+#Hagamos un programa que determine si eres mayor de edad o no
+
+# COMMAND ----------
+
+edad = int(input("Ingresa tu edad: "))
+
+
+# COMMAND ----------
+
+edad = 35
+
+# COMMAND ----------
+
+if edad > 18:
+    print("Eres mayor de edad")
+else:
+    print("No eres mayor de edad")
+
+# COMMAND ----------
+
+
