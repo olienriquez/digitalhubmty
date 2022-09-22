5063b277cb551e3609c0eb6e1470e2c0d0e74711
olienriquez <olienriquez>
2022-09-22 01:11:14 +0000

    Changed file Compras por cliente.py

--- Compras por cliente.py
+++ Compras por cliente.py
@@ -1,30 +1,42 @@
 # Databricks notebook source
-# MAGIC %md
-# MAGIC Crear un programa que permita al usuario ingresar el tiempo dentro en un estacionamiento , cortando el ingreso de datos cuando el usuario ingresa 0 minutos.
-# MAGIC Si ingresa una cantidad negativa, no debe procesarse y se le debe solicitar que ingrese una nueva cantidad.
-# MAGIC Ingresar una tarifa fija durante la primera hora (60 minutos) de $25 y $15 por cada hora adicional. Al finalizar, informar el total a pagar teniendo en cuenta que, si el monto supera las 8 horas se aplica una tarifa fija de $200 extra.
+tiempo = float(input("Ingresa tiempo de estancia en minutos: "))
+
 
 # COMMAND ----------
 
-tiempo = float(input("Ingresa el tiempo dentro del estacionamiento en minutos:"))
+tiempo = float(input("Ingresa tiempo de estancia en minutos: "))
 
-# COMMAND ----------
+while tiempo <= 0:
+    if tiempo == 0 :
+        
+        break
+    else: 
+        tiempo = float(input("Ingresa un tiempo válido:"))
 
-if tiempo == 0:
-    print("Total a pagar: 0")
-elif tiempo < 0:
-    float(input("Ingresa un tiempo válido:"))
-elif tiempo < 60:
+if tiempo > 0 and tiempo <=60:
     tarifa = 25
-    print(f"Total a pagar:{tarifa}")
-else:
+    print("Total a pagar:",tarifa)
+
+
+elif tiempo > 60:
+    
     if tiempo%60 == 0:
         tarifa = 25 + ((tiempo // 60)-1) * 15
     else:
         tarifa = 25 + ((tiempo // 60)) * 15
-        
-    print(f"Total a pagar:{tarifa}")
+
+    print("Total a pagar:",tarifa)
     
+
+
+# COMMAND ----------
+
+tiempo
+
+
+# COMMAND ----------
+
+((tiempo // 60)-1) * 15
 
 # COMMAND ----------
 
